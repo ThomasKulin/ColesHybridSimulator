9518b9b7ff7fa0702423e971aa981779ef909176
15JGME <15jgme@queensu.ca>
2020-04-26 14:11:25 -0400

    Innitial files

---
+++ RegRate.m
@@ -0,0 +1,50 @@
+%% ENPH 455 Hybrid Rocket Motor Design Project (Iterative Code)
+%Written by: Cole Mero 
+%October 29, 2019
+%
+%function [regRate, m_fuel, G_tot] = RegRate(m_ox, m_fuel, currentD, rho, Lp, dt) 
+%
+%Takes in the oxidizer mass flow rate, fuel mass flow rate, the current
+%diameter, the density of the solid fuel, the length of the combustion
+%chamber, and the differential time step. Returns the instantaneous
+%regression rate, fuel mass flow rate, and propellant mass flux. 
+
+%Note that references to SPAD are the textbook Space Propuslsion Analysis
+%and Design by Humble. 
+
+%% Function to Calculate Instantaneous Regression Rate + Fuel Mass Flow Rate
+
+function [regRate, m_fuel, G_tot] = RegRate(m_ox, currentD, rho, Lp)
+
+% a = 2.066*10^(-5);   %[kg^(-n)m^(1+2n-m)s^(n-1)] (reference value found on pg 385 of SPAD) 
+% n = 0.75;              %(reference value found on pg 385 of SPAD) 
+% m = -0.15;             %(reference value found on pg 385 of SPAD) 
+% 
+% G_tot = (m_ox + m_fuel)/currentA; %[kg/m^2*s] Calculate the instantaneous total mass flux 
+% 
+% 
+% regRate = a*(G_tot^n)*Lp^m; %[m/s] Calcualte the instantaneous regression rate
+% m_fuel = regRate*rho*pi*currentD*Lp*(dt*regRate); %[kg/s] Calculate the instantaneous fuel mass flow rate
+
+currentA = pi*(currentD/2)^2;  %[m^2]      Calculate the current combustion port cross sectional area
+
+%The below constants are derived for rather specific and stupid units for
+%the terms in the regression rate law, example values for these constants
+%can be found in SPAD Table 7.2 Pg 389. Typically these values are found
+%from experimental data, thus I used a research paper and played with the
+%values to emulate their results. Research paper:
+%https://pdfs.semanticscholar.org/b004/f9574f56c810273f5bbf7e494ad3598abdae.pdf
+
+Ao = 0.000007; 
+No = 0.8; 
+Mo = -0.20;
+
+Gox = (m_ox/currentA); %[kg/m^2*s] Oxidizer flux in units for the regression rate calc;
+regRate = Ao*(Gox^No)*(Lp^Mo); %[m/s] Regression rate calculation (eqn 7.36 from SPAD pg 385)
+
+m_fuel = regRate*rho*(pi*currentD*Lp); %[kg/s] Instantaneous fuel mass flow rate
+Gfuel = m_fuel/currentA; %[kg/m^2*s] Instantaneous fuel mass flux 
+
+G_tot = Gox + Gfuel; %Calculates the instantaneous total mass flux 
+
+end 
---
+++ rocketModel.m
@@ -0,0 +1,108 @@
+function [score] = rocketModel(t1D,t1T, mcT, mcF)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+% calculate thrust curve from motor parameters
+%Variables for Mini-Hybrid Prediction (comment or uncomment) 
+
+finalD = t1D-t1T-mcT;     %[m]    Maximum possible diameter of the motor after completed burn 
+initialD = 0.05*finalD;  %[m]    Initial diameter of combustion port, pre-burn 
+Lp = 0.121;          %[m]    Length of the combustion port 
+m_ox = 0.0123;       %[kg/s] Oxidizer flow rate (experimentally measured)
+throatR = 0.0014;    %[m]    Radius of the nozzle throat
+exitR = 0.00455;     %[m]    Radius of the nozzle exit 
+
+%Caclulated initial variables for simulation 
+A_t = pi*(throatR)^2; %[m^2]  Specify the nozzle throat area 
+A_e = pi*(exitR)^2;   %[m^2]  Calculate the nozzle exit area
+epsilon = A_e/A_t;    %Nozzle area expansion ratio
+
+rho = 975;           %[kg/m^3]  Average density of ABS plastic 
+m_fuel = 0;          %[kg/s]  Fuel flow rate (intialized to 0 for simulation)
+lamda = 0.97;        %Nozzle efficiency 
+Pa = 101325;         %[Pa] Ambient pressure 
+R = 8314.41/29.19;   %[J/kmol*K] Universal gas constant divided by MM of combustion products
+dt = 0.2;            %[s] Differential time step to be used for each iteration
+G_tot = [];          %Initialize G_tot array to store the instantaneous G values
+
+
+%% Begin Simulation 
+currentD = initialD; %To begin set the diameter to initial (unburnt) diameter
+
+j = 1;
+
+while currentD < finalD
+
+%Calculate the regression rate using custom RegRate function 
+[regRate, m_fuel, G_tot(j)] = RegRate(m_ox, currentD, rho, Lp); 
+ 
+%Calculate the new combustion port diameter
+currentD = currentD + 2*regRate*dt %Unmuted to check progress/speed of sim
+
+%Calculate the total propellant mass flow
+m_tot = m_ox + m_fuel; 
+
+%Caculate the OF ratio 
+OF_ratio(j) = m_ox/m_fuel; 
+
+%Using the OF ratio, consult paper
+%https://pdfs.semanticscholar.org/a15a/a1b29fffeebfcdb70965e4241af07eb5f00f.pdf
+%to get the characteristic velocity for this OF ratio OR use RPA and derive
+%them all yourself (this is a good way to do it) and it will also yield the
+%flame temp, ratio of specific heats, and molecular mass of the
+%combustion products
+
+%Values from RPA for research paper (uncomment or comment)
+c_star = 1605;      %[m/s] 
+gamma = 1.1708;     %Ratio of specific heats (Cp and Cv idk which order) from RPA
+molMass = 0.002919; %[kg/mol] Average molar mass of the output exhaust
+Tc = 3301.8;        %[K] Flame temperature (also used as combustion chamber stagnation temp)
+ 
+%Values from RPA for mini-hybrid  (uncomment or comment)
+% c_star = 1559;      %[m/s] 
+% gamma = 1.2015;     %Ratio of specific heats (Cp and Cv idk which order) from RPA
+% molMass = 0.002992; %[kg/mol] Average molar mass of the output exhaust
+% Tc = 3321.8;        %[K] Flame temperature (also used as combustion chamber stagnation temp)
+
+%Calcualte the pressure in combustion chamber in Pascals and PSI
+pressure_Chamber_Pa(j) =  c_star*m_tot/A_t; %[Pa]
+Pc(j) = pressure_Chamber_Pa(j); 
+pressure_Chamber_PSI = pressure_Chamber_Pa(j)*0.000145038; %[PSI]
+
+%Calcualte the exit Mach number, to do so use eqn 3.100 from SPAD
+%(Humble)and solve it numerically 
+syms x
+Me = double(vpasolve(epsilon^(2*(gamma-1)/(gamma+1)) == (2/(1+gamma))*...
+    (x)^(-2*(gamma-1)/(gamma+1)) + ((gamma-1)/2)*x^(2*(1-...
+    ((gamma-1)/(gamma+1)))), x)); 
+
+%Calculate the exit pressure, use eqn 3.95 from SPAD (Humble), with the
+%previously determined chamber pressure as the stagnation pressure 
+Pe = Pc(j)*(1+((gamma-1)/2)*Me^2)^-(gamma/(gamma-1)); %[Pa]
+
+%Calculate the exit exhaust temperature to determine the exit velocity,
+%given by eqn 3.94 from SPAD (Humble)
+Te = ((1 + (((gamma-1)/2)*Me^2))^-1)*Tc; 
+
+%Caclualte the exit velocity using eqn 3.112 from SPAD (Humble), using the 
+%exit temperature from above
+Ve = sqrt(((2*gamma*R*Te)/(gamma-1))*(1-(Pe/Pc(j)))^((gamma-1)/gamma)); %[m/s]
+
+%Now calculate the theoretical thrust of the motor using eqn 1.6 from SPAD
+thrust(j) = lamda*(m_tot*Ve + (Pe-Pa)*A_e); %[N]
+
+if j == 1
+    time(j) = 0; %Ensure simulation starts at t=0
+else
+    time(j) = time(j-1) + dt;
+end
+
+j = j+1; 
+end
+
+
+
+
+
+end
+
